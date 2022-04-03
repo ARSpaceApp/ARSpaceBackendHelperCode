4c54cebf9a83a9f03080bd79f2cc7a2f918267fe
vegiwoo <vegiwoo@protonmail.com>
2022-04-03 18:30:11 +0300

    Checking and rebuilding a package

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ARSpaceBackendHelperCode",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v10_15), .iOS(.v13)],
     products: [
         .library(
             name: "ARSpaceBackendHelperCode",
